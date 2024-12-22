4b4be074a8354b7e960ff0e9570d8660ecbd0d22
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2024-12-22 18:50:42 +0530

    gg

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const http = require('node:http');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+function IST_date() {
+    const options = {
+        timeZone: 'IST',
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    };
+    const ist_date = new Date(new Date().toLocaleString('en-US', options));
+
+    return ist_date;
+}
+
 const server = http.createServer((req, res) => { // Server create karna
     axios.get("https://satta-king-fast.com")
     .then(response => {
@@ -15,16 +31,61 @@ const server = http.createServer((req, res) => { // Server create karna
         const new_date = new_el_children.first().attr('title');
         const old_results = old_el_children.not(':first-child');
         const new_results = new_el_children.not(':first-child');
-        const obj = { date: { old: old_date, new: new_date } };
+        const obj = {}; // sk results
         
         for (let i=0; i<4; i++) obj[names.eq(i).html().trim()] = { old: old_results.eq(i).html().trim(), new: new_results.eq(i).html().trim() };
 
         if (!req.headers['user-agent'].includes('Mozilla')) {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(obj));
-            return;
+            if (!Object.hasOwn(req.headers, 'full-data')) { // Return type text
+                const date = IST_date();
+                const hours = date.getHours();
+                // const minutes = date.getMinutes();
+
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+                if (hours >= 18 && hours < 21) { // faridabad
+                    let result = obj["FRBD"]["new"];
+                    if (result == "XX") {
+                        res.end("no-data");
+                        return;
+                    }
+                    res.end(`Faridabad ${result}`);
+                    return;
+                } else if (hours => 21 && hours < 0) { // gaziabad
+                    let result = obj["GZBD"]["new"];
+                    if (result == "XX") {
+                        res.end("no-data");
+                        return;
+                    }
+                    res.end(`Gaziabad ${result}`);
+                    return;
+                } else if (hours => 0 && hours < 4) { // gali
+                    let result = obj["GALI"]["new"];
+                    if (result == "XX") {
+                        res.end("no-data");
+                        return;
+                    }
+                    res.end(`Gali ${result}`);
+                    return;
+                } else { // all, inc. disawer
+                    let results = [];
+                    for (let key in obj) {
+                        let res = obj[key]['new'];
+                        if (res == "XX") res = obj[key]['old'] + " *"
+                        results.push(`${key} ${res}`);
+                    }
+                    res.end(results.join("\n"));
+                    return;
+                }
+            } else { // Return type json
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(obj));
+                return;
+            }
         }
         
+        obj["date"] = { old: old_date, new: new_date }
+
         let text = '<!doctypehtml><html lang=en><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><title>SK results</title><style>tr{border-bottom:1px solid #ddd}table{width:95vw}th{text-align:left}</style><body>';
         let header = "<tr>";
         let neww = header;
